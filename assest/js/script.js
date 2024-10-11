844ceec686b76ada63ee0a8a4fc7b76d8dd07083
Caio Luiz da Silva <caio.silva.3@aluno.cefet-rj.br>
2024-10-11 18:27:59 -0300

    add_task

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,26 +1,38 @@
-document.querySelectorAll('.kanban-card').forEach(card => {
-    card.addEventListener('dragstart', e => {
-        e.currentTarget.classList.add('dragging');
+document.addEventListener('DOMContentLoaded', () => {
+    // Função para adicionar nova tarefa
+    document.querySelectorAll('.add-card').forEach(button => {
+        button.addEventListener('click', (event) => {
+            const column = event.target.closest('.kanban-column');
+            const taskTitle = prompt('Digite o título da nova tarefa:');
+
+            if (taskTitle) {
+                const cardHTML = `
+                    <div class="kanban-card" draggable="true">
+                        <div class="badge medium">
+                            <span>Medium priority</span>
+                        </div>
+                        <p class="card-title">${taskTitle}</p>
+                        <div class="card-infos">
+                            <div class="card-icons">
+                                <p>
+                                    <i class="fa-regular fa-comment"></i> 0
+                                </p>
+                                <p>
+                                    <i class="fa-solid fa-paperclip"></i> 0
+                                </p>
+                            </div>
+                            <div class="user">
+                                <img src="assest/images/avatar.avif" alt="Avatar">
+                            </div>
+                        </div>
+                        <button class="delete-card">Excluir</button>
+                    </div>
+                `;
+                column.querySelector('.kanban-cards').innerHTML += cardHTML;
+                addDeleteEventListeners();
+                addCommentListeners();
+            }
+        });
     });
 
-    card.addEventListener('dragend', e => {
-        e.currentTarget.classList.remove('dragging');
-    });
-});
-
-document.querySelectorAll('.kanban-cards').forEach(column => {
-    column.addEventListener('dragover', e => {
-        e.preventDefault();
-        e.currentTarget.classList.add('cards-hover');
-    });
-
-    column.addEventListener('dragleave', e => {
-        e.currentTarget.classList.remove('cards-hover');
-    });
-
-    column.addEventListener('drop', e => {
-        e.currentTarget.classList.remove('cards-hover');
-        const dragCard = document.querySelector('.kanban-card.dragging');
-        e.currentTarget.appendChild(dragCard);
-    });
 });
